2e3a2b5b81ec0c14f6c997294024f89862fdc11c
Amina786billiona <amina786billiona@gmail.com>
2026-07-03 17:42:42 +0500

    Add template literals practice

---
+++ template-literals.js
@@ -0,0 +1,32 @@
+        //    Template Literals (Template Strings)
+
+
+let name="amina";
+let age=22;
+console.log(`my name is ${name} ,I am ${age} year old`);
+
+// 2. Expressions Inside ${}
+
+const a=10;
+const b=20;
+console.log(`sum is = ${a+b}`)
+
+// 3. Multi-line Strings
+
+const mess=`I
+LOVE
+PAKISTAN`;
+console.log(mess);
+
+// Example: Student Details
+
+const student = {
+    name: "Amina",
+    age: 22,
+    course: "MERN"
+};
+console.log(`
+    name:${student.name}
+    age:${student.age}
+    course:${student.course}`)
+
