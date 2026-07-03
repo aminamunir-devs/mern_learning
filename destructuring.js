3bfd91272cea9ac85b2a3b0376d8b00bc8bc33b7
Amina786billiona <amina786billiona@gmail.com>
2026-07-03 15:20:05 +0500

    Add destructuring practice

---
+++ destructuring.js
@@ -0,0 +1,122 @@
+        /*  What is Destructuring?
+
+Destructuring means unpacking values from an array or object into separate variables. */
+
+/* 
+There are two types of destructuring
+Object Destructuring
+Array Destructuring 
+*/
+
+// 1. Object Destructuring
+
+const student = {
+    name: "Amina",
+    age: 22,
+    course: "MERN"
+};
+const {name,age,course}=student;
+console.log(name);
+console.log(age);
+console.log(course);   // Variable names must match
+
+// Renaming Variables
+
+const person = {
+    name: "Amina",
+    age: 22
+};
+
+const {name:fullname}=person;
+console.log(fullname);
+
+// Default Values
+
+const students = {
+    names: "Amina"
+};
+const {names,city="lahore"}=students;
+console.log(name);
+console.log(city);
+
+// 2. Array Destructuring
+
+/* Arrays don't use property names.
+They use positions (indexes). */
+
+// With destructuring
+
+const color=["red","blue","green"];
+const [first,second,third]=color;
+console.log(first);
+console.log(second);
+console.log(third);
+
+// Skip Values
+
+const numbers=[10,20,30,40];
+const [firsts, ,thirds]=numbers;
+console.log(firsts);
+console.log(thirds);
+
+// Default Values in Arrays
+
+const num=[10];
+const [a,b=20]=num;
+console.log(a);
+console.log(b);
+
+// Nested Object Destructuring
+
+const stud={
+    nam:"amina",
+    addres:{
+        cityy:"lahore",
+        country:"pakistan"
+    }
+};
+const {addres:{cityy,country}}=stud;
+console.log(cityy);
+console.log(country);
+
+// Nested Array Destructuring
+
+const nums=[10,[20,30]];
+const [m,[n,c]]=nums;
+console.log(m);
+console.log(n);
+console.log(c);
+
+            //  Destructuring Function Parameters
+
+        
+/* 
+Without destructuring
+
+function display(student) {
+    console.log(student.name);
+    console.log(student.age);
+}
+
+display({
+    name: "Amina",
+    age: 22
+});
+ */
+
+// With destructuring
+
+const display=({name,age})=>
+{
+    console.log(name);
+    console.log(age);
+}
+display(
+    {
+        name:"amina",
+        age:22
+    }
+)
+
+
+
